02eb7ea6a50f2a41df9709acf18b3bf03e14f154
pranavlonari <pranavlonari12@gmail.com>
2023-12-15 15:56:31 +0530

    modified code for functionality and added remove option below list item

--- components/App.jsx
+++ components/App.jsx
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [inputValue, setInputValue] = useState(" ");
+  const [items, setItems] = useState([]);
+
+  function handleInputChange(event) {
+    setInputValue(event.target.value);
+  }
+
+  function handleAddItem() {
+    if (inputValue.trim() !== "") {
+      setItems([...items, inputValue]);
+      setInputValue("");
+    }
+  }
+
+  function handleRemoveItem(index) {
+    const updatedItems = [...items];
+    updatedItems.splice(index, 1);
+    setItems(updatedItems);
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter a new item"
+        />
+        <button onClick={handleAddItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {items.map((item, index) => (
+            <li key={index}>
+              {item}
+              <div>
+                <button onClick={() => handleRemoveItem(index)}>Remove</button>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
